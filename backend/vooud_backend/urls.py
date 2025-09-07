3d40cbd2c6cf6f5381a814a83f160a8bfb64d941
b2ygroup <lionrisingb2y@gmail.com>
2025-09-07 14:40:06 -0300

    FIX: Remove importação fantasma da ApiRootView

--- backend/vooud_backend/urls.py
+++ backend/vooud_backend/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from operations.views import ApiRootView
 
 urlpatterns = [
-    # A URL raiz não deve ter 'api/' aqui, pois já estamos no contexto da API.
-    path('', ApiRootView.as_view(), name='api-root'), 
-
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/operations/', include('operations.urls')),
